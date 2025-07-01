907e8fb9b2d0666bb63b6fd413601539341c2fb6
keitheous <i.am.keith.chong@gmail.com>
2025-07-02 09:09:35 +1000

    Create project repository

---
+++ lib/term_deposit_calculator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "term_deposit_calculator/version"
+
+module TermDepositCalculator
+  class Error < StandardError; end
+  # Your code goes here...
+end
